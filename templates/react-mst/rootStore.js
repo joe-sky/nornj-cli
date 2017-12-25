65142ed754efa9ee4e0abfceeccaa76ed195a3a0
joe_sky <hitomi_doax@sina.com>
2017-12-25 20:03:55 +0800

    Fixed bugs in windows

--- templates/react-mst/rootStore.js
+++
@@ -1,9 +0,0 @@
-﻿<#-template name="importStore">
-import #{pageName | pascal}#Store from "./pages/#{pageName}#Store";
-//{importStore}//
-</#-template>
-
-<#-template name="pageStore">
-#{pageName}#: types.optional(#{pageName | pascal}#Store, {}),
-  //{pageStore}//
-</#-template>
